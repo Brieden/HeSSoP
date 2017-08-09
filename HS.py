6938589a97d911c5960a500532da94139a5bf41e
Brieden <oreja@posteo.de>
2017-08-09 23:13:45 +0200

    nada

--- HS.py
+++ HS.py
@@ -18,7 +18,7 @@ import matplotlib.pyplot as plt
 #    return vx, vy, vz, ax, ay, az, bx, by, bz, ex, ey, ez
 
 ###############################################################################
-#                                Parameter                                    #
+#                                Parameters                                   #
 # Set the paramater:                                                          #
 #       number of gridpoints:   N_x, N_y, N_z                                 #
 #       number of timesteps:    N_t                                           #
@@ -26,7 +26,7 @@ import matplotlib.pyplot as plt
 #       gamma:                                                                #
 #       mu:                                                                   #
 ###############################################################################
-N_x, N_y, N_z, N_t = 90, 60, 60, 5
+N_x, N_y, N_z, N_t = 100, 100, 100, 5
 x_min, x_max, y_min, y_max, z_min, z_max = -0.5, 0.5, -0.5, 0.5, 0, 5
 gamma = 5/3
 mu = 1
@@ -75,18 +75,18 @@ if 1:
     s2 = 0.4
     s3 = 0.2
     k1 = 1
-    k2 = k1**(-(c1 + c2)*a_s)
+    k2 = k1*np.exp(-(c1 + c2)*a_s)
     x01 = 0.0
 
 #   while over all
 #      z = dble(k)*dz
     
-    p0 = s1**(-s2*Z)+s3 
-    p = p0
+    p0 = s1*np.exp(-s2*Z)+s3 
+    p[:] = p0
     
     xsep = ((1/c1)*np.sqrt(2/p0)
-    *np.arctanh(np.sqrt((p0 - k1*np.exp(-c1*a_s))/p0)))
-#    *datanh(dsqrt((p0 - k1*np.exp(-c1*as))/p0),val)   
+           *np.arctanh(np.sqrt((p0 - k1*np.exp(-c1*a_s))/p0)))
+#          *datanh(dsqrt((p0 - k1*np.exp(-c1*as))/p0),val)   
     
     p1 = np.exp(s2*Z)*s3+s1
     p2 = np.exp(a_s*c1+s2*Z)
@@ -110,7 +110,7 @@ if 1:
     jy[0,X<xsep] = c1*p0[X<xsep]/(np.cosh(c1*X[X<xsep]*np.sqrt(p0[X<xsep]/2)))
 #    else X>=xsep
     x02[X>=xsep] = (np.arctanh(np.sqrt((p0[X>=xsep] - k1*np.exp(-c1*a_s))/p0[X>=xsep]))*
-            np.sqrt(2/p0[X>=xsep])*(1/c1 + 1/c2))
+                    np.sqrt(2/p0[X>=xsep])*(1/c1 + 1/c2))
     ay[0,X>=xsep]= (-(2/c2)*np.log(np.cosh(c2*np.sqrt(p0[X>=xsep]/2)
                     *(X[X>=xsep] - x02[X>=xsep])))+(1/c2)*np.log(p0[X>=xsep]/k2))
     bx[0,X>=xsep] = ((s1*s2*(2*np.sqrt((2*p3[X>=xsep])/(p3[X>=xsep]*s3+s1))*
@@ -120,28 +120,26 @@ if 1:
                     + np.sqrt((2*p3[X>=xsep])/(p3[X>=xsep]*s3+s1))*
                     np.sinh(p8[X>=xsep]/c1)*c1*c2*p6[X>=xsep]*s1*X[X>=xsep]
                     - 2*np.sinh(p8[X>=xsep]/c1)*c1*p3[X>=xsep]
-                    -2*np.sinh(p8[X>=xsep]/c1)*c2*p3[X>=xsep]))/
+                    - 2*np.sinh(p8[X>=xsep]/c1)*c2*p3[X>=xsep]))/
                     (2*np.sqrt((2*p3[X>=xsep])/(p3[X>=xsep]*s3+s1))*
-                     np.cosh(p8[X>=xsep]/c1)*c1*c2*p3[X>=xsep]*p5[X>=xsep]*p6[X>=xsep]*(p3[X>=xsep]*s3+s1)))
+                     np.cosh(p8[X>=xsep]/c1)*c1*c2*p3[X>=xsep] *
+                     p5[X>=xsep]*p6[X>=xsep]*(p3[X>=xsep]*s3+s1)))
     bz[0,X>=xsep] = (2*np.sinh(p8[X>=xsep]/c1)*p5[X>=xsep])/np.cosh(p8[X>=xsep]/c1)
     jy[0,X>=xsep] = c2*p0[X>=xsep]/(np.cosh(c2*X[X>=xsep]*np.sqrt(p0[X>=xsep]/2)))
         
         
 #        endif     für alle
-    ax[:]  = 0
-    az[:]  = 0
+    ax[:] = 0
+    az[:] = 0
     by[:] = 0
     gauss[:] = 0
     rho[:] = p[:]*np.exp(-Z/ho)
     jx[:] = 0
     jz[:] = 0
-    energy[:] = (p[:]/(gamma - 1)
-                        + (bx[:]*bx[:]
-                        + by[:]*by[:]
-                        + bz[:]*bz[:])/(2*mu))
+    energy[:] = (p[:]/(gamma - 1) + (bx[:]*bx[:] + by[:]*by[:]+ bz[:]*bz[:])/(2*mu))
     eta[:] = (eta0*np.exp(-a*(Z-zcusp))*
                     np.tanh(abs(jy[:])))
-#                       np.tanh(abs(dble(xjy(n,i,j,k))),val)
+#                   np.tanh(abs(dble(xjy(n,i,j,k))),val)
     ex[:] = 0
     ey[:] = eta[:]*jy[:]
     ez[:] = 0   
@@ -149,6 +147,6 @@ if 1:
 ###############################################################################
 #                                  plotting                                   #
 ###############################################################################
-plt.contour(bx[0,int(N_x/2)])
+plt.contour(bx[0,int(N_x/2)], 40)#, extent=(-0.5, 0.5, 0, 5))
 plt.show()
 
